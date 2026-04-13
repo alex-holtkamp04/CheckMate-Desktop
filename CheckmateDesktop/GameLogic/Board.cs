eb113bfb7aa0a538178f16e9f8e9bc7dca911a22
alex-holtkamp04 <2004ajh@gmail.com>
2026-04-12 23:32:21 -0500

    Added setupBoard() function (untested)

--- CheckmateDesktop/GameLogic/Board.cs
+++ CheckmateDesktop/GameLogic/Board.cs
@@ -1,4 +1,5 @@
 ﻿using CheckmateDesktop.GameLogic;
+using CheckmateDesktop.GameLogic.Pieces;
 using System;
 using System.Collections.Generic;
 using System.IO.Pipelines;
@@ -18,7 +19,113 @@ namespace CheckmateDesktop
         public Board()
         {
             ActivePlayer = TeamColor.White;
-            BoardSquares = new Piece[8, 8];   
+            BoardSquares = new Piece[8, 8];
+            setupBoard();
+        }
+
+        private void setupBoard()
+        {
+            for (int index = 0; index < 64; index++)
+            {
+                int row = index / 8;
+                int col = index % 8;
+                switch (row)
+                {
+                    case 0:
+                        if (col == 0 || col == 7)
+                        {
+                            BoardSquares[row, index] = new Rook
+                            {
+                                Team = Piece.TeamColor.Black
+                            };
+                        }
+                        if (col == 1 || col == 6)
+                        {
+                            BoardSquares[row, index] = new Knight
+                            {
+                                Team = Piece.TeamColor.Black
+                            };
+                        }
+                        if (col == 2 || col == 5)
+                        {
+                            BoardSquares[row, index] = new Bishop
+                            {
+                                Team = Piece.TeamColor.Black
+                            };
+                        }
+                        if (col == 3)
+                        {
+                            BoardSquares[row, index] = new Queen
+                            {
+                                Team = Piece.TeamColor.Black
+                            };
+                        }
+                        if (col == 4)
+                        {
+                            BoardSquares[row, index] = new King
+                            {
+                                Team = Piece.TeamColor.Black
+                            };
+                        }
+                        break;
+
+                    case 1:
+                        BoardSquares[row, index] = new Pawn
+                        {
+                            Team = Piece.TeamColor.Black
+                        };
+                        break;
+
+                    case 6:
+                        BoardSquares[row, index] = new Pawn
+                        {
+                            Team = Piece.TeamColor.White
+                        };
+                        break;
+
+                    case 7:
+                        if (col == 0 || col == 7)
+                        {
+                            BoardSquares[row, index] = new Rook
+                            {
+                                Team = Piece.TeamColor.White
+                            };
+                        }
+                        if (col == 1 || col == 6)
+                        {
+                            BoardSquares[row, index] = new Knight
+                            {
+                                Team = Piece.TeamColor.White
+                            };
+                        }
+                        if (col == 2 || col == 5)
+                        {
+                            BoardSquares[row, index] = new Bishop
+                            {
+                                Team = Piece.TeamColor.White
+                            };
+                        }
+                        if (col == 3)
+                        {
+                            BoardSquares[row, index] = new Queen
+                            {
+                                Team = Piece.TeamColor.White
+                            };
+                        }
+                        if (col == 4)
+                        {
+                            BoardSquares[row, index] = new King
+                            {
+                                Team = Piece.TeamColor.White
+                            };
+                        }
+                        break;
+
+                    default:
+                        BoardSquares[row, index] = null;
+                        break;
+                }
+            }
         }
     }
 }
